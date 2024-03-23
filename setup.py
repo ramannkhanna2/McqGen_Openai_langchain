9d629f905f7a6d504ed49bc411b84c9a56c65cad
raman <raman@mail.com>
2024-03-23 15:04:37 +0530

    changed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='raman khanna',
+    author_email='raman@email.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
